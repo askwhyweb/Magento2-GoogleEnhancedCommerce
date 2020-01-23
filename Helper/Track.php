ae0e513ebc537e00d9d4d2c404420bbbdd59c96e
Farhan Islam <farhan@orvisoft.com>
2020-01-23 17:11:16 +0500

    updates

--- Helper/Track.php
+++ Helper/Track.php
@@ -7,13 +7,100 @@ use Magento\Framework\App\Helper\AbstractHelper;
 
 class Track extends AbstractHelper
 {
-
+    protected $scopeConfig;
+    protected $_registry;
+    protected $_categoryCollectionFactory;
+    protected $_productRepository;
     /**
      * @param \Magento\Framework\App\Helper\Context $context
      */
     public function __construct(
-        \Magento\Framework\App\Helper\Context $context
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory,
+        \Magento\Catalog\Model\ProductRepository $productRepository,
+        \Magento\Framework\Registry $registry
     ) {
         parent::__construct($context);
+        $this->scopeConfig = $scopeConfig;
+        $this->_registry = $registry;
+        $this->_categoryCollectionFactory = $categoryCollectionFactory;
+        $this->_productRepository = $productRepository;
     }
+
+    public function isActive(){
+        return (boolean) $this->scopeConfig->getValue('enhanced_ecommerce/options/is_active', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getAnalyticsId(){
+        return $this->scopeConfig->getValue('enhanced_ecommerce/options/analytics_id', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+    public function getCurrentCategory(){
+        return $this->_registry->registry('current_category');
+    }
+
+    public function getCurrentProduct(){
+        return $this->_registry->registry('current_product');
+    }
+
+    public function getBrand($_product){
+        $brandAttribute = $this->scopeConfig->getValue('enhanced_ecommerce/options/brand', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $outputValue = $_product->getAttributeText($brandAttribute);
+        if(strlen($outputValue)){
+            $outputValue = addslashes($outputValue);
+            return ",'brand': '$outputValue'";
+        }
+        return '';
+    }
+
+    /**
+     * Get category collection
+     *
+     * @param bool $isActive
+     * @param bool|int $level
+     * @param bool|string $sortBy
+     * @param bool|int $pageSize
+     * @return \Magento\Catalog\Model\ResourceModel\Category\Collection or array
+     */
+    public function getCategoryCollection($isActive = true, $level = false, $sortBy = false, $pageSize = false)
+    {
+        $collection = $this->_categoryCollectionFactory->create();
+        $collection->addAttributeToSelect('*');        
+        
+        // select only active categories
+        if ($isActive) {
+            $collection->addIsActiveFilter();
+        }
+                
+        // select categories of certain level
+        if ($level) {
+            $collection->addLevelFilter($level);
+        }
+        
+        // sort categories by some value
+        if ($sortBy) {
+            $collection->addOrderField($sortBy);
+        }
+        
+        // select certain number of categories
+        if ($pageSize) {
+            $collection->setPageSize($pageSize); 
+        }    
+        
+        return $collection;
+    }
+
+    public function getCategoryName($_product){
+        $categoryIds = $_product->getCategoryIds(); 
+        $categories = $this->getCategoryCollection(true, 0)
+                            ->addAttributeToFilter('entity_id', $categoryIds);
+                            
+        foreach ($categories as $category) {
+            $catName = $category->getName();
+            return ", 'category': '$catName'";
+        }
+        return '';
+    }
+
 }
